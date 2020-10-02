94e0e922c267b6daf866e10eb4a3f4b4ebe6f8c4
Shivamsingh3238 <Shivamsingh3238>
2020-10-02 16:38:29 +0530

    method

--- short_cut(method)/set_operation.py
+++ short_cut(method)/set_operation.py
@@ -5,12 +5,14 @@ import sys
 m=int(input())
 arr=set(list(map(int,input().split())))
 com=itertools.combinations(arr,m)
-print(dir(com))
+#print(dir(com))
 ##min=-sys.maxsize
 ##for obj in com:
 ##    pair=itertools.combinations(obj,2)
 ##    for x,y in pair:
 ##        dif==abs(x-y)
 ##        
-        
-        
+for x,y in com:
+    print(x,end="")
+    print(y,end="")
+    print()
